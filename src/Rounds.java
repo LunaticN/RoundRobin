c07a05b73365e4bedd09ed736f19e9b5d7cf1e51
Naya Antaki <88854675+LunaticN@users.noreply.github.com>
2023-11-13 13:38:01 -0500

    Update Rounds.java

--- src/Rounds.java
+++ src/Rounds.java
@@ -3,12 +3,17 @@ import java.util.Collections;
 
 public class Rounds {
     ArrayList<Integer> matches;
+    int rounds;
+    int mpr;
+
 
     public Rounds(ArrayList<Integer> matches, ArrayList<String> teams){
         this.matches = matches;
+        this.rounds = teams.size() - 1;
+        this.mpr = (rounds + 1) / 2;
     }
 
-    public ArrayList<ArrayList<Integer>> intoRounds(){
+    public ArrayList<ArrayList<ArrayList<Integer>>> intoRounds(){
         ArrayList<ArrayList<Integer>> roundSets = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> pairs = new ArrayList<Integer>();
         for (int i = 0; i <= matches.size() - 1; i+=2) {
@@ -20,13 +25,13 @@ public class Rounds {
         Collections.shuffle(roundSets);
         ArrayList<ArrayList<ArrayList<Integer>>> tournament = new ArrayList<ArrayList<ArrayList<Integer>>>();
         ArrayList<ArrayList<Integer>> roundAtHand = new ArrayList<ArrayList<Integer>>();
-        System.out.println(roundSets);
+        //System.out.println(roundSets);
         int multi = 0;
         //iterate through roundSets with counter, append to tournament list when counter = mpr
         for (int i = 0; i < rounds; i++) {
             for (int j = 0; j < mpr; j++) {
                 roundAtHand.add(roundSets.get(j + multi));
-                System.out.println(roundAtHand);
+                //System.out.println(roundAtHand);
             }
             tournament.add(new ArrayList<>(roundAtHand));
             multi+=mpr;
